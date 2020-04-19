ea7846cbd5c100eda62ed8f2483a79caf82bf1bd
Thomas <1210280984@qq.com>
2020-04-19 06:14:04 +0000

    显示头像

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -23,9 +23,9 @@
                     @else
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
-                                {{ Auth::user()->name }}
-                            </a>
+              <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+              {{ Auth::user()->name }}
+            </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
               <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
