66732c0c8a8c3cdf591ce017de9de15afa4eb025
Thomas <1210280984@qq.com>
2020-04-19 05:15:48 +0000

    “编辑个人资料”

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -27,16 +27,16 @@
                                 {{ Auth::user()->name }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="">个人中心</a>
-                                <a class="dropdown-item" href="">编辑资料</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" id="logout" href="#">
-                                    <form action="{{ route('logout') }}" method="POST">
-                                        {{ csrf_field() }}
-                                        <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                                    </form>
-                                </a>
-                            </div>
+              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
+              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
+            </div>
                         </li>
                         @endguest
             </ul>
