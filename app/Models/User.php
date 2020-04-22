6b9c60aaf7f6deb479ea7cba5c9479afda17c8cc
Thomas <1210280984@qq.com>
2020-04-22 05:16:55 +0000

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 { 
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use MustVerifyEmailTrait;
